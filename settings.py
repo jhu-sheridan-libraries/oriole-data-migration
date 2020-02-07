968c276402069f9351ccd8bc8dce7dcefd71c626
Amanda <amanda@MSEL-LIAPMAC06.local>
2020-02-07 13:46:42 -0500

    new tags from script

--- settings.py
+++ settings.py
@@ -4,25 +4,25 @@ import os
 import requests
 import json
 
-load_dotenv(verbose=True)
+#load_dotenv(verbose=True)
 
-ORIOLE_API_ROOT     = os.getenv('ORIOLE_API_ROOT')
-ORIOLE_API_USERNAME = os.getenv('ORIOLE_API_USERNAME')
-ORIOLE_API_PASSWORD = os.getenv('ORIOLE_API_PASSWORD')
-ORIOLE_API_TENANT   = os.getenv('ORIOLE_API_TENANT')
+#ORIOLE_API_ROOT     = os.getenv('ORIOLE_API_ROOT')
+#ORIOLE_API_USERNAME = os.getenv('ORIOLE_API_USERNAME')
+#ORIOLE_API_PASSWORD = os.getenv('ORIOLE_API_PASSWORD')
+#ORIOLE_API_TENANT   = os.getenv('ORIOLE_API_TENANT')
 
-OKAPI_ENABLED = distutils.util.strtobool(os.getenv('OKAPI_ENABLED'))
+ORIOLE_API_ROOT     = "http://https://lara-test.library.jhu.edu"
+ORIOLE_API_USERNAME = "diku_admin"
+ORIOLE_API_PASSWORD = "admin"
+ORIOLE_API_TENANT   = "diku"
 
-SS_USERNAME = os.getenv('SERIAL_SOLUTIONS_USERNAME')
-SS_PASSWORD = os.getenv('SERIAL_SOLUTIONS_PASSWORD')
-SS_WSDL = os.getenv('SERIAL_SOLUTIONS_WSDL')
-SS_LIBRARY_CODE = os.getenv('SERIAL_SOLUTIONS_LIBRARY_CODE')
+#OKAPI_ENABLED = distutils.util.strtobool(os.getenv('OKAPI_ENABLED'))
+OKAPI_ENABLED = True
 
-
-def build_headers():
-    headers = {'x-okapi-tenant': ORIOLE_API_TENANT, 'content-type': 'application/json'}
-    if OKAPI_ENABLED:
-        payload = {'username': ORIOLE_API_USERNAME, 'password': ORIOLE_API_PASSWORD}
-        response = requests.post(f'{ORIOLE_API_ROOT}/authn/login', data=json.dumps(payload), headers=headers, verify=False)
-        headers['Authorization'] = f"Bearer {response.headers['x-okapi-token']}"
-    return headers
+headers = {'x-okapi-tenant': ORIOLE_API_TENANT, 'content-type': 'application/json'}
+if OKAPI_ENABLED:
+    print("1");
+    payload = {'username': ORIOLE_API_USERNAME, 'password': ORIOLE_API_PASSWORD}
+    response = requests.post(f'{ORIOLE_API_ROOT}/authn/login', data=json.dumps(payload), headers=headers, verify=False)
+    headers['Authorization'] = f"Bearer {response.headers['x-okapi-token']}"
+    print(str(headers))
